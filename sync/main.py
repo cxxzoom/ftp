3aaefc66e53a42e051273103197836f8a7097e75
ucdo <phpsarc@gmail.com>
2023-10-11 09:46:58 +0800

    update

--- sync/main.py
+++ sync/main.py
@@ -1,5 +1,21 @@
 import os
 import yaml
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mapping():
+    dir_ = yaml_config
+    
+    # mappings = {}
+    # scan(dir_,mappings)
+    # print("hello world!")
+    # return mappings
+    return "hello world"
+
+
 
 def scan(path, my_dir):
     files = os.listdir(path)
@@ -11,7 +27,7 @@ def scan(path, my_dir):
 
         if os.path.isdir(tmp):
             print("this is dir->%s", tmp)
-            scan(tmp, mydir)
+            scan(tmp, my_dir)
 
 
 def yaml_config():
@@ -20,16 +36,7 @@ def yaml_config():
     # 打开YAML文件并解析内容
     with open(yaml_file_path, 'r') as yaml_file:
         yaml_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    return yaml_data if yaml_data else {}
 
-    print(yaml_data)
-
-
-my_dir = {}
-# scan("F:\\script_boy", my_dir)
-
-dire = {}
-
-print(dire)
-
-yaml_config()
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999)
